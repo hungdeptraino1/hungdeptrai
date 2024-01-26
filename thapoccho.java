6935d41d55f36f55feb422a2e6055dd258a765e6
hungdeptraino1 <97597945+hungdeptraino1@users.noreply.github.com>
2024-01-26 13:22:16 +0700

    Add files via upload

---
+++ thapoccho.java
@@ -0,0 +1,12 @@
+package baitap;
+import java.util.Scanner;
+public class thapoccho {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        for(int i=1 ;i<=n ;i++){
+
+        }
+    
+    }
+}
